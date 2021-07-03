4c0b4ddacb717279ad3b0d40418342b61fed743f
godzillaman <86501263+godzillaman@users.noreply.github.com>
2021-07-02 21:20:58 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+
+function setup() 
+{
+  createCanvas(400,400);
+  avaerage()
+}
+
+function draw() 
+{
+background(51);
+
+}
+function avaerage(){
+  var w=[20,30,40]
+  var sum =w[0]+w[1]+w[2]
+  var abga=sum/3
+  console.log(abga)
+}
